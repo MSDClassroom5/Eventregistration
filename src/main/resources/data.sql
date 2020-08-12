efb00d7e57a5d1714812f318c21a9a44897695c6
Chris Leung _ <leung_chris@bah.com>
2020-08-11 20:22:21 -0400

    update data.sql and schema.sql for Events and Registration

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,16 @@ insert into CUSTOMERS (NAME, EMAIL, PASSWORD) VALUES ('Raj', 'raj@b.com','passwo
 insert into CUSTOMERS (NAME, EMAIL, PASSWORD) VALUES ('Ryan', 'ryan@c.com','password');
 insert into CUSTOMERS (NAME, EMAIL, PASSWORD) VALUES ('Sharika', 'sharika@d.com','password');
 insert into CUSTOMERS (NAME, EMAIL, PASSWORD) VALUES ('Teresa', 'teresa@e.com','password');
+
+insert into EVENTS (CODE, TITLE, DESCRIPTION) VALUES ('E1', 'Weekly Meeting','Weekly meeting #1');
+insert into EVENTS (CODE, TITLE, DESCRIPTION) VALUES ('E2', 'Sales Meeting','Sales meeting FY2020');
+insert into EVENTS (CODE, TITLE, DESCRIPTION) VALUES ('E3', 'Fun Meeting','Happy Happy Meeting');
+insert into EVENTS (CODE, TITLE, DESCRIPTION) VALUES ('E4', 'Covid Meeting','Trash Fire FY2020');
+insert into EVENTS (CODE, TITLE, DESCRIPTION) VALUES ('E5', 'Advertising Meeting','Social Media Meeting');
+
+insert into REGISTRATIONS (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES) VALUES (1, 1, '2019-01-11 00:00:00.0','notes1');
+insert into REGISTRATIONS (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES) VALUES (2, 2, '2018-01-11 00:00:00.0','notes2');
+insert into REGISTRATIONS (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES) VALUES (3, 3, '2017-01-11 00:00:00.0','notes3');
+insert into REGISTRATIONS (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES) VALUES (4, 4, '2016-01-11 00:00:00.0','notes4');
+insert into REGISTRATIONS (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES) VALUES (5, 5, '2015-01-11 00:00:00.0','notes5');
+
