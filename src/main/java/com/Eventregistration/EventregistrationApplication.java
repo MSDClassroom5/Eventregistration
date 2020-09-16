2810b2cb189159ee5ca6c9ce58092d6aa1697caa
user <user@this.com>
2020-09-16 10:12:25 -0400

    Added more logs.

--- src/main/java/com/Eventregistration/EventregistrationApplication.java
+++ src/main/java/com/Eventregistration/EventregistrationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EventregistrationApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MSD Project group 5::Starting API application");
 		SpringApplication.run(EventregistrationApplication.class, args);
 	}
 
